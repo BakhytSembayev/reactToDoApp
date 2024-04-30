9b85150cee8f34d850c3c38c07c5604be2347b56
Bakhyt Sembayev <bahytsembaev@gmail.com>
2024-04-30 07:43:07 +0500

    json-server

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import './App.css';
+import _ from 'lodash';
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+  const [search, setSearch] = useState('');
+  const [sortByAlpha, setSortByAlpha] = useState(false);
 
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/todos')
+    axios.get('http://localhost:5000/todos')
       .then(response => {
         setTodos(response.data);
       })
       .catch(error => console.error('Error fetching data: ', error));
   }, []);
 
+  const addTodo = () => {
+    axios.post('http://localhost:5000/todos', { title: input })
+      .then(response => {
+        setTodos([...todos, response.data]);
+        setInput('');
+      });
+  };
+
+  const deleteTodo = (id) => {
+    axios.delete(`http://localhost:5000/todos/${id}`)
+      .then(() => {
+        setTodos(todos.filter(todo => todo.id !== id));
+      });
+  };
+
+ const updateTodo = (id) => {
+    const newTitle = prompt('Edit the todo item:');
+    axios.put(`http://localhost:5000/todos/${id}`, { title: newTitle })
+      .then(response => {
+        setTodos(todos.map(todo => todo.id === id ? response.data : todo));
+      });
+  };
+
+  const debouncedSearch = useCallback(_.debounce((searchTerm) => {
+    setSearch(searchTerm);
+  }, 300), []);
+
+  const handleSearch = (event) => {
+    debouncedSearch(event.target.value);
+  };
+
+  const toggleSort = () => {
+    setSortByAlpha(!sortByAlpha);
+  };
+
+  const displayedTodos = todos
+    .filter(todo => todo.title.toLowerCase().includes(search.toLowerCase()))
+    .sort((a, b) => sortByAlpha ? a.title.localeCompare(b.title) : 0);
+
   return (
     <div className="App">
       <h1>Todo List</h1>
+      <input 
+        type="text" 
+        placeholder="Add new todo" 
+        value={input} 
+        onChange={(e) => setInput(e.target.value)} 
+      />
+      <button onClick={addTodo}>Add</button>
+      <input 
+        type="text" 
+        placeholder="Search todos" 
+        onChange={handleSearch}
+      />
+      <button onClick={toggleSort}>Sort Alphabetically: {sortByAlpha ? 'On' : 'Off'}</button>
       <ul>
-        {todos.map(todo => (
-          <li key={todo.id}>{todo.title}</li>
+        {displayedTodos.map(todo => (
+          <li key={todo.id}>
+            {todo.title}
+            <button onClick={() => updateTodo(todo.id)}>Edit</button>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
         ))}
       </ul>
     </div>
